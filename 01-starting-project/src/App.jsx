78537c07a9033a2ff7aa54d1e9177ad577cad255
yurim <y1551463@gmail.com>
2025-04-05 20:19:34 +0900

    컴포넌트, 동적 값 출력

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,19 +1,29 @@
+const reactDescriptions = ["기본적인", "중요한", "핵심적인"];
+
+function genRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+    const description = reactDescriptions[genRandomInt(2)];
+    return (
+        <header>
+            <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+            <h1>React Essentials</h1>
+            <p>모든 앱 제작에 필요한 {description} React 개념</p>
+        </header>
+    );
+}
+
 function App() {
-  return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
+    return (
+        <div>
+            <Header></Header>
+            <main>
+                <h2>시작하기!</h2>
+            </main>
+        </div>
+    );
 }
 
 export default App;
