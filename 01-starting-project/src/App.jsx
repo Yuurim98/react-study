9c72741b00c4537730cbbd543bdf3d3d8b7b01d9
yurim <y1551463@gmail.com>
2025-04-05 20:59:23 +0900

    이미지 파일 import

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,3 +1,4 @@
+import reactImg from "./assets/react-core-concepts.png";
 const reactDescriptions = ["기본적인", "중요한", "핵심적인"];
 
 function genRandomInt(max) {
@@ -8,7 +9,7 @@ function Header() {
     const description = reactDescriptions[genRandomInt(2)];
     return (
         <header>
-            <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+            <img src={reactImg} alt="Stylized atom" />
             <h1>React Essentials</h1>
             <p>모든 앱 제작에 필요한 {description} React 개념</p>
         </header>
