1847b4126ca7da0ac78135907e72c162e7008381
yurim <y1551463@gmail.com>
2025-04-08 22:40:10 +0900

    개별 파일로 분리

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,32 +1,6 @@
-import reactImg from "./assets/react-core-concepts.png";
 import { CORE_CONCEPTS } from "./data.js";
-
-const reactDescriptions = ["기본적인", "중요한", "핵심적인"];
-
-function genRandomInt(max) {
-    return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-    const description = reactDescriptions[genRandomInt(2)];
-    return (
-        <header>
-            <img src={reactImg} alt="Stylized atom" />
-            <h1>React Essentials</h1>
-            <p>모든 앱 제작에 필요한 {description} React 개념</p>
-        </header>
-    );
-}
-
-function CoreConcept({ image, title, description }) {
-    return (
-        <li>
-            <img src={image} alt={title} />
-            <h3>{title}</h3>
-            <p>{description}</p>
-        </li>
-    );
-}
+import Header from "./components/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
 
 function App() {
     return (
---
+++ 01-starting-project/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import reactImg from "../assets/react-core-concepts.png";
+
+const reactDescriptions = ["기본적인", "중요한", "핵심적인"];
+
+function genRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+export default function Header() {
+    const description = reactDescriptions[genRandomInt(2)];
+    return (
+        <header>
+            <img src={reactImg} alt="Stylized atom" />
+            <h1>React Essentials</h1>
+            <p>모든 앱 제작에 필요한 {description} React 개념</p>
+        </header>
+    );
+}
