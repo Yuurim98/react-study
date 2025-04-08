8b5be8c51a7b90a0c0d58c9952cc4ddd789b86c8
yurim <y1551463@gmail.com>
2025-04-08 22:50:25 +0900

    Header 폴더 지정

--- 01-starting-project/src/components/Header/Header.jsx
+++ 01-starting-project/src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
-import reactImg from "../assets/react-core-concepts.png";
+import reactImg from "../../assets/react-core-concepts.png";
+import "./Header.css";
 
 const reactDescriptions = ["기본적인", "중요한", "핵심적인"];
 
