02872fff2758be650955cfbfc42dda58806c7708
yurim <y1551463@gmail.com>
2025-04-20 21:02:46 +0900

    useState를 활용하여 UI 업데이트

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
+
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TapButton from "./components/TapButton.jsx";
 
 function App() {
+    const [selectedTopic, setSelectedTopic] = useState("버튼을 클릭하세요");
+
     function handleSelect(selectedButton) {
         console.log(selectedButton + "클릭");
+        setSelectedTopic(selectedButton);
     }
+
+    console.log("app 컴포넌트 렌더링");
 
     return (
         <div>
@@ -37,6 +44,7 @@ function App() {
                             State
                         </TapButton>
                     </menu>
+                    {selectedTopic}
                 </section>
             </main>
         </div>
