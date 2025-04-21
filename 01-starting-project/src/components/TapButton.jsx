81516d4dd203620a1f134a9ff38ded5ac0963ff5
yurim <y1551463@gmail.com>
2025-04-21 17:48:11 +0900

    DOM에 전달되지 않도록 조건 수정

--- 01-starting-project/src/components/TapButton.jsx
+++ 01-starting-project/src/components/TapButton.jsx
@@ -1,7 +1,10 @@
 export default function TapButton({ children, onSelect, isSelected }) {
     return (
         <li>
-            <button className={isSelected && "active"} onClick={onSelect}>
+            <button
+                className={isSelected ? "active" : undefined}
+                onClick={onSelect}
+            >
                 {children}
             </button>
         </li>
